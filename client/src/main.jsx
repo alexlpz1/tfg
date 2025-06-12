b66d633f522c26dec44b49ce7fd194e94da79c08
Alex López <a.lopez@itgranada.com>
2025-06-12 12:32:15 +0200

    actualizado221

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,18 @@
+// src/main.jsx
 import React from 'react'
-import ReactDOM from 'react-dom/client'   // <<< IMPORTA ReactDOM
+import ReactDOM from 'react-dom/client'
 import App from './App'
 import { ThemeProvider, createGlobalStyle } from 'styled-components'
-import { colors, breakpoints } from './theme'
+import { colors, breakpoints } from './theme'   // <-- importa también breakpoints
 
-// tus estilos globales…
-const GlobalStyle = createGlobalStyle`…`
+const GlobalStyle = createGlobalStyle`
+  *,*::before,*::after{box-sizing:border-box;margin:0;padding:0}
+  html,body,#root{height:100%;background:${p=>p.theme.colors.background};color:${p=>p.theme.colors.text};font-family:'Orbitron',sans-serif}
+`
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(
-  <React.StrictMode>
-    <ThemeProvider theme={{ colors, breakpoints }}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ThemeProvider theme={{ colors, breakpoints }}>  {/* <-- aquí le pasas ambos */}
+    <GlobalStyle/>
+    <App/>
+  </ThemeProvider>
 )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -6,6 +6,7 @@ import styled from 'styled-components';
 import heroImg from '../assets/hero.png';
 
 
+
 const Container = styled.main`
   display: flex;
   flex-direction: column;
